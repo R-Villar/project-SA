6f2b5ed30be91ecd70972a1222e3fc8fd0783820
Remberto Villar <rembertovillar@gmail.com>
2023-04-21 23:12:52 -0400

    updated postfeed.

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,7 @@
 import Post from "../models/Post.js";
 import User from "../models/User.js";
+import Comment from "../models/Comment.js";
+import { getPostComments } from "./comments.js";
 
 // CREATE
 export const createPost = async (req, res) => {
@@ -29,7 +31,17 @@ export const createPost = async (req, res) => {
 // READ
 export const getFeedPosts = async (req, res) => {
 	try {
-		const post = await Post.find();
+		const post = await Post.find()
+
+        // const postWithComments = await Promise.all(post.comments.map((id) => {
+        //     if (post.comments) {
+        //         Comment.findById(id)
+        //     }
+        // }))
+
+        // console.log(postWithComments)
+
+
 		res.status(200).json(post);
 	} catch (err) {
 		res.status(404).json({ message: err.message });

17c6498b05cca55609692546c931914c03acb19b
Remberto Villar <rembertovillar@gmail.com>
2023-05-05 13:01:46 -0400

    updated comments with delete route and controller.

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createComment, getPostComments, updateContent } from "../controllers/comments.js";
+import { createComment, getPostComments, updateContent, deleteComment } from "../controllers/comments.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
@@ -12,5 +12,8 @@ router.post("/:postId", verifyToken, createComment);
 
 // PATCH
 router.patch("/:commentId", verifyToken, updateContent);
+
+//DELETE
+router.delete("/:postId/:commentId", verifyToken, deleteComment);
 
 export default router;

7a17688cf66ce6dbfb4f24471086636e798bc03f
Remberto Villar <rembertovillar@gmail.com>
2023-05-15 11:30:16 -0400

    setup notistack.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,20 @@ import App from "./App";
 import "./index.css";
 import authReducer from "./state";
 import { Provider } from "react-redux";
-import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { PersistGate } from "redux-persist/integration/react";
 import { configureStore } from "@reduxjs/toolkit";
+import { SnackbarProvider } from "notistack";
 
 const persistConfig = { key: "root", storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, authReducer);
@@ -25,7 +35,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<PersistGate loading={null} persistor={persistStore(store)}>
-				<App />
+				<SnackbarProvider autoHideDuration={3000} hideIconVariant>
+					<App />
+				</SnackbarProvider>
 			</PersistGate>
 		</Provider>
 	</React.StrictMode>
